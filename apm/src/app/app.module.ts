d5061e35f800e2537d356445cdfab4aa8f21691d
Tom Victor <vjtomvictor@icloud.com>
2021-04-29 14:18:43 +0530

    Custom pipe

--- apm/src/app/app.module.ts
+++ apm/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { productListComponent } from './products/product-list.component';
+import { ConvertToSpacePipe } from './shared/convert-to-spaces.pipe'
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    productListComponent
+    productListComponent,
+    ConvertToSpacePipe
   ],
   imports: [
     BrowserModule,
