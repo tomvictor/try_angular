85dfac0f97693b6130e001db9d6aef0819096277
Tom Victor <vjtomvictor@icloud.com>
2021-05-01 11:33:35 +0530

    of and from operator from rxjs

--- RxjsDemo/src/app/app.component.ts
+++ RxjsDemo/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'RxjsDemo';
+
+  ngOnInit(){
+    console.log("init...");
+    of(2,4,6,8).subscribe(console.log);
+    from([20,15,10,5]).subscribe(
+      item => console.log(`resulting item.. ${item}`), //back tick operator for ts template strings (string interpoletion)
+      err => console.log(`error occured ${err}`),
+      () => console.log("completed")
+    )
+  }
 }
