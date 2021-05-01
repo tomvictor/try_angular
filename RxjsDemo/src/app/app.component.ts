0aed9ca70fb25f4b025a460a9ea89eaa99630c16
Tom Victor <vjtomvictor@icloud.com>
2021-05-01 11:49:05 +0530

    map operator

--- RxjsDemo/src/app/app.component.ts
+++ RxjsDemo/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { from, of } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,18 @@ export class AppComponent implements OnInit {
   title = 'RxjsDemo';
 
   ngOnInit(){
-    console.log("init...");
+    console.log("of function");
     of(2,4,6,8).subscribe(console.log);
+    console.log("from function");
     from([20,15,10,5]).subscribe(
       item => console.log(`resulting item.. ${item}`), //back tick operator for ts template strings (string interpoletion)
       err => console.log(`error occured ${err}`),
       () => console.log("completed")
     )
+    console.log("map operator");
+    of(2,4,6).pipe(
+      map(item => item*2)
+    ).subscribe(console.log)
+
   }
 }
